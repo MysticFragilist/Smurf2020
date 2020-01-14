8370fdf69aab670a11f018ecd396b3a1dc80799e
Samuel Montambault <amos.samuel@hotmail.com>
2020-01-14 17:17:07 -0500

    Initial commit

---
+++ Assets/Scripts/CameraNetworkInit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Cinemachine;
+public class CameraNetworkInit : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void setTarget(Transform target)
+    {
+        this.GetComponent<CinemachineVirtualCamera>().Follow = target;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+
+    [SyncVar]
+    float horizontalMove = 0f;
+    [SyncVar]
+    bool crouch = false;
+    [SyncVar]
+    bool jump = false;
+    public int ID { get; private set; } = 0; 
+
+    private static int conpteID = 0;
+
+    public override void OnStartLocalPlayer()
+     {
+         Camera.main.GetComponent<CameraNetworkInit>().setTarget(this.gameObject.transform);
+     }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        conpteID ++;
+        if (this.GetComponent<NetworkIdentity>().isLocalPlayer) 
+        {
+            this.ID = conpteID;
+            Debug.Log("This ID " + this.ID + " is created");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.GetComponent<NetworkIdentity>().isLocalPlayer)
+            return;
+        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding() 
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    private void FixedUpdate() {
+        if (!this.GetComponent<NetworkIdentity>().isLocalPlayer)
+            return;
+        
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        jump = false;
+    }
+}
