d0c705e8179b3634cedb8c77ee7b958b23283865
Camille Brulotte <camille.brulotte@icloud.com>
2020-01-14 20:12:15 -0500

    Menu
    Ajout du slider dans les options

--- Assets/Options.cs
+++ Assets/Options.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
+    public 
+
+    public void SetVolume(float volume)
+    {
+
+    }
+
     public void retour()
     {
         Debug.Log("retour");
