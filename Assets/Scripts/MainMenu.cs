9da3dd2bd2b1473224cc0bafefc6257071bbf139
Camille Brulotte <camille.brulotte@icloud.com>
2020-01-14 19:37:46 -0500

    Menu
    Ajout du font

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Jouer()
+    {
+        Debug.Log("Jouer");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void quitter()
+    {
+        Debug.Log("Quitter");
+        Application.Quit();
+    }
+}
